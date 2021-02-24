904b99c102538a61149c42cd677e65f37feddc05
Igor M <iggorm@yahoo.com.br>
2021-02-24 17:11:53 -0300

    implementando repository

---
+++ lojaDeGamesGeneration/src/main/java/org/generation/lojaDeGamesGeneration/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package org.generation.lojaDeGamesGeneration.repository;
+
+import java.util.List;
+
+import org.generation.lojaDeGamesGeneration.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository  extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+}
