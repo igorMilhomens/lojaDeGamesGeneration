cfd839866b60bd30bac9fd158efc478111368a82
Igor M <iggorm@yahoo.com.br>
2021-02-24 17:17:57 -0300

    implementando model

---
+++ lojaDeGamesGeneration/src/main/java/org/generation/lojaDeGamesGeneration/model/Usuario.java
@@ -0,0 +1,60 @@
+package org.generation.lojaDeGamesGeneration.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nomeCompleto;
+	
+	@NotNull
+	private String email;
+	
+	@NotNull
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
