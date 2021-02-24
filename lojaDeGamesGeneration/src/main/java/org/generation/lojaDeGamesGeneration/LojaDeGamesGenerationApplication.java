a3dab1611eb04595a6b43a4c16b9ad864c8f04ca
Igor M <iggorm@yahoo.com.br>
2021-02-24 17:02:01 -0300

    implementando model

---
+++ lojaDeGamesGeneration/src/main/java/org/generation/lojaDeGamesGeneration/LojaDeGamesGenerationApplication.java
@@ -0,0 +1,13 @@
+package org.generation.lojaDeGamesGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeGamesGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeGamesGenerationApplication.class, args);
+	}
+
+}
---
+++ lojaDeGamesGeneration/src/test/java/org/generation/lojaDeGamesGeneration/LojaDeGamesGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.lojaDeGamesGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
